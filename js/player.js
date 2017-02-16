e14b80c1c70f9b8fbab8f1fb88418acd97f29e33
Juan Polanco <jspolancor@gmail.com>
2017-02-15 21:56:30 -0500

    Creates Player class

--- js/player.js
+++ js/player.js
@@ -1,17 +1,96 @@
 function Player () {
-  //create a sprite and add the 3 animations
-  this.sprite = createSprite(400, 200, 50, 100),
-  this.create = function(){
-    var myAnimation = this.sprite.addAnimation("floating", "assets/ghost_standing0001.png", "assets/ghost_standing0007.png");
-    myAnimation.offY = 18;
 
-    this.sprite.addAnimation("moving", "assets/ghost_walk0001.png", "assets/ghost_walk0004.png");
-  },
-  this.moveTo = function(x, y){
+  this.sprite = createSprite(400, 200, 100, 100);
+  this.sprite.addAnimation("standing", "assets/player/player02.png");
+  this.sprite.addAnimation("moving-up", "assets/player/player19.png", "assets/player/player24.png");
+  this.sprite.addAnimation("moving-down", "assets/player/player01.png", "assets/player/player06.png");
+  this.sprite.addAnimation("moving-left", "assets/player/player07.png", "assets/player/player12.png");
+  this.sprite.addAnimation("moving-right", "assets/player/player13.png", "assets/player/player18.png");
+  this.sprite.addAnimation("moving-up-right", "assets/player/player43.png", "assets/player/player48.png");
+  this.sprite.addAnimation("moving-up-left", "assets/player/player37.png", "assets/player/player42.png");
+  this.sprite.addAnimation("moving-down-left", "assets/player/player30.png", "assets/player/player25.png");
+  this.sprite.addAnimation("moving-down-right", "assets/player/player31.png", "assets/player/player36.png");
 
-  },
-  this.position = {
-    x: this.sprite.position.x,
-    y: this.sprite.position.y,
+  // Move the player around
+  this.move = function(){
+
+    if(keyIsDown(RIGHT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 0);
+      this.sprite.changeAnimation('moving-right');
+    }
+
+    if(keyIsDown(DOWN_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 90);
+      this.sprite.changeAnimation('moving-down');
+    }
+
+    if(keyIsDown(LEFT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 180);
+      this.sprite.changeAnimation('moving-left');
+    }
+
+    if(keyIsDown(UP_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 270);
+      this.sprite.changeAnimation('moving-up');
+    }
+
+    if(keyIsDown(DOWN_ARROW) && keyIsDown(RIGHT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 45);
+      this.sprite.changeAnimation('moving-down-right');
+    }
+
+    if(keyIsDown(DOWN_ARROW) && keyIsDown(LEFT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 135);
+      this.sprite.changeAnimation('moving-down-left');
+    }
+
+    if(keyIsDown(UP_ARROW) && keyIsDown(RIGHT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 315);
+      this.sprite.changeAnimation('moving-up-right');
+    }
+
+    if(keyIsDown(UP_ARROW) && keyIsDown(LEFT_ARROW)){
+      this.sprite.setSpeed(playerSpeed, 225);
+      this.sprite.changeAnimation('moving-up-left');
+    }
+
+    //set the camera position to the players position
+    if (this.sprite.position.x > 0 && this.sprite.position.x < SCENE_W ){
+      camera.position.x = this.sprite.position.x;
+    }
+
+    if (this.sprite.position.y > 0 && this.sprite.position.y < SCENE_H){
+      camera.position.y = this.sprite.position.y;
+    }
+
+    // Set boundaries of the game
+    if(this.sprite.position.y > SCENE_H + windowHeight / 2 - 25){
+      this.sprite.position.y = SCENE_H + windowHeight / 2 - 25;
+    }
+
+    if(this.sprite.position.y < windowHeight / 2 * -1 + 25){
+      this.sprite.position.y = windowHeight / 2 * -1 + 25;
+    }
+
+    if(this.sprite.position.x > SCENE_W + windowWidth / 2 - 25){
+      this.sprite.position.x = SCENE_W + windowWidth / 2 - 25;
+    }
+
+    if(this.sprite.position.x < windowWidth / 2 * -1 + 25){
+      this.sprite.position.x = windowWidth / 2 * -1 + 25;
+    }
+
   }
+
+  // Stops all animations
+  this.keyReleased = function(){
+    this.sprite.setSpeed(0, 0);
+    this.sprite.changeAnimation('standing');
+    return false;
+  }
+
+  this.dash = function(){
+
+  }
+
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,91 +1,45 @@
-//virtual camera
-//move the mouse around
-//the sprite follows the mouse but appears at the center of the sketch
-//because the camera is following it
-
 var player;
 var bg;
 var playerSpeed = 8;
+var cameraZoom = 1;
 //scene size
 var SCENE_W = 2000;
 var SCENE_H = 2000;
-
-var goToPosition = {
-  x: 0,
-  y: 0
-}
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   bg = new Group();
   //create some background for visual reference
-  for(var i=0; i<50; i++)
+  for(var i=0; i<100; i++)
   {
-    //create a sprite and add the 3 animations
     var rock = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height));
-    //cycles through rocks 0 1 2
     rock.addAnimation("normal", "assets/rocks"+i%3+".png");
     bg.add(rock);
   }
 
-  player = createSprite(400, 200, 50, 100);
-  var myAnimation = player.addAnimation("floating", "assets/ghost_standing0001.png", "assets/ghost_standing0007.png");
-  myAnimation.offY = 18;
-  player.addAnimation("moving", "assets/ghost_walk0001.png", "assets/ghost_walk0004.png");
+  player = new Player;
 
 }
 
 function draw() {
+
   background(255,255,255);
-  camera.zoom = .5;
-
-  if(keyIsDown(RIGHT_ARROW))
-    player.setSpeed(playerSpeed, 0);
-  if(keyIsDown(DOWN_ARROW))
-    player.setSpeed(playerSpeed, 90);
-  if(keyIsDown(LEFT_ARROW))
-    player.setSpeed(playerSpeed, 180);
-  if(keyIsDown(UP_ARROW))
-    player.setSpeed(playerSpeed, 270);
-  if(keyIsDown(DOWN_ARROW) && keyIsDown(RIGHT_ARROW))
-    player.setSpeed(playerSpeed, 45);
-  if(keyIsDown(DOWN_ARROW) && keyIsDown(LEFT_ARROW))
-    player.setSpeed(playerSpeed, 135);
-  if(keyIsDown(UP_ARROW) && keyIsDown(RIGHT_ARROW))
-    player.setSpeed(playerSpeed, 315);
-  if(keyIsDown(UP_ARROW) && keyIsDown(LEFT_ARROW))
-    player.setSpeed(playerSpeed, 225);
-  //set the camera position to the players position
-  if (player.position.x > 0 && player.position.x < SCENE_W )
-    camera.position.x = player.position.x;
-  if (player.position.y > 0 && player.position.y < SCENE_H)
-    camera.position.y = player.position.y;
-    console.log(player.position.y)
-  if(player.position.y > SCENE_H + 300 + windowHeight / 2)
-    player.position.y = SCENE_H + 300 + windowHeight / 2;
-    //camera.off();
-  //if(player.position.x > SCENE_W)
-    //player.position.x = SCENE_W;
-    //camera.off();
-  //if(player.position.y > SCENE_H)
-    //player.position.y = SCENE_H;
-    //camera.off();
-
+  camera.zoom = cameraZoom;
+  player.move();
   fill(255);
   noStroke();
   drawSprites();
-
   camera.off();
+
 }
 
 function keyReleased() {
-  player.setSpeed(0, 0);
-  return false;
+  player.keyReleased();
 }
 
 function keyPressed(){
-  if(keyCode == 32)
-    console.log('dash');
+  if(keyCode == 32){
+    player.dash();
+  }
 }
