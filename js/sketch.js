961da4eaeb37b2d75094d56b9fa47fe871bd884c
Juan Polanco <jspolancor@gmail.com>
2017-02-22 00:09:51 -0500

    Modifies firebase auth state change

--- js/sketch.js
+++ js/sketch.js
@@ -14,39 +14,63 @@ var text1;
 var style = { font: "10px", fill: "#00ff44" };
 var weapon;
 var fireButton;
+var isAuth = false;
+
+function initGame(){
+
+}
 
 function create() {
+  game.add.tileSprite(0, 0, 1920, 1920, 'background');
+  game.world.setBounds(0, 0, 1920, 1920);
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  player = game.add.sprite(game.world.centerX, game.world.centerY, 'ms');
+  text1 = game.add.text(0, 0, playerName, style);
+  player.animations.add('standing', [0,1], false);
+  player.animations.add('walk-down', [0,1,2,3,4,5], false);
+  player.animations.add('walk-left', [6,7,8,9,10,11], false);
+  player.animations.add('walk-right', [12,13,14,15,16,17], false);
+  player.animations.add('walk-up', [18,19,20,21,22,23], false);
+  player.animations.add('walk-down-left', [24,25,26,27,28,29], false);
+  player.animations.add('walk-down-right', [30,31,32,33,34,35], false);
+  player.animations.add('walk-up-left', [36,37,38,39,40,41], false);
+  player.animations.add('walk-up-right', [42,43,44,45,46,47], false);
+  game.physics.p2.enable(player);
+  //game.physics.arcade.enable(player);
+  player.body.fixedRotation = true;
+  weapon = game.add.weapon(8, 'bullet');
+  weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
+  weapon.bulletSpeed = 600;
+  weapon.fireRate = 300;
+  weapon.trackSprite(player, 5, 5, false);
 
-    game.add.tileSprite(0, 0, 1920, 1920, 'background');
-    game.world.setBounds(0, 0, 1920, 1920);
-    game.physics.startSystem(Phaser.Physics.P2JS);
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'ms');
-    playerName = 'Paquito';
-    text1 = game.add.text(0, 0, playerName, style);
-    player.animations.add('standing', [0,1], false);
-    player.animations.add('walk-down', [0,1,2,3,4,5], false);
-    player.animations.add('walk-left', [6,7,8,9,10,11], false);
-    player.animations.add('walk-right', [12,13,14,15,16,17], false);
-    player.animations.add('walk-up', [18,19,20,21,22,23], false);
-    player.animations.add('walk-down-left', [24,25,26,27,28,29], false);
-    player.animations.add('walk-down-right', [30,31,32,33,34,35], false);
-    player.animations.add('walk-up-left', [36,37,38,39,40,41], false);
-    player.animations.add('walk-up-right', [42,43,44,45,46,47], false);
-    game.physics.p2.enable(player);
-    //game.physics.arcade.enable(player);
-    player.body.fixedRotation = true;
-    weapon = game.add.weapon(20, 'bullet');
-    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-    weapon.bulletSpeed = 600;
-    weapon.fireRate = 200;
-    weapon.trackSprite(player, 0, 0, false);
+  cursors = game.input.keyboard.createCursorKeys();
+  game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
     fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-    cursors = game.input.keyboard.createCursorKeys();
-    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+    firebase.auth().onAuthStateChanged(function(user) {
+
+      if (user) {
+        playerName = user.displayName
+
+        console.log(playerName);
+        text1.text = playerName;
+        $('#modalRegister').modal('hide');
+        $('#modalLogin').modal('hide');
+      } else {
+        $('#modalRegister').modal('toggle');
+      }
+
+    });
+
+
 
 }
 
 function update() {
+
+    if (!isAuth) {
+        //return;
+    }
 
     player.body.setZeroVelocity();
 
