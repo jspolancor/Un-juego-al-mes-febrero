5f9c3c6b91b6d4208e22e67e2ee0488016ac33bf
Juan Polanco <jspolancor@gmail.com>
2017-02-23 16:34:35 -0500

    Moves player movement to the players class

--- js/player.js
+++ js/player.js
@@ -1,193 +1,101 @@
-function Player (id, nickname) {
+function Player (id, nick) {
 
-  this.id = null;
-  this.nickname = null;
+  this.nick = nick;
+  this.id = id;
+  this.sprite = null;
 
-  if(id != null){
-    this.id = id;
-  }
+  this.create = function(){
 
-  if(nickname != null){
-    this.nickname = nickname;
-  }
-  this.bulletImage = loadImage("assets/bullet.png");
-  this.direction = 9;
+    this.nickStyle = {
+        font: "10px",
+        fill: "#00ff44"
+    };
 
-  this.sprite = createSprite(400, 200, 100, 100);
-  this.sprite.addAnimation("standing", "assets/player/player02.png");
-  this.sprite.addAnimation("moving-up", "assets/player/player19.png", "assets/player/player24.png");
-  this.sprite.addAnimation("moving-down", "assets/player/player01.png", "assets/player/player06.png");
-  this.sprite.addAnimation("moving-left", "assets/player/player07.png", "assets/player/player12.png");
-  this.sprite.addAnimation("moving-right", "assets/player/player13.png", "assets/player/player18.png");
-  this.sprite.addAnimation("moving-up-right", "assets/player/player43.png", "assets/player/player48.png");
-  this.sprite.addAnimation("moving-up-left", "assets/player/player37.png", "assets/player/player42.png");
-  this.sprite.addAnimation("moving-down-left", "assets/player/player30.png", "assets/player/player25.png");
-  this.sprite.addAnimation("moving-down-right", "assets/player/player31.png", "assets/player/player36.png");
+    this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'ms');
+    this.sprite.animations.add('standing', [0, 1], false);
+    this.sprite.animations.add('walk-down', [0, 1, 2, 3, 4, 5], false);
+    this.sprite.animations.add('walk-left', [6, 7, 8, 9, 10, 11], false);
+    this.sprite.animations.add('walk-right', [12, 13, 14, 15, 16, 17], false);
+    this.sprite.animations.add('walk-up', [18, 19, 20, 21, 22, 23], false);
+    this.sprite.animations.add('walk-down-left', [24, 25, 26, 27, 28, 29], false);
+    this.sprite.animations.add('walk-down-right', [30, 31, 32, 33, 34, 35], false);
+    this.sprite.animations.add('walk-up-left', [36, 37, 38, 39, 40, 41], false);
+    this.sprite.animations.add('walk-up-right', [42, 43, 44, 45, 46, 47], false);
+    //this.sprite.body.fixedRotation = true;
 
-  // Move the player around
-  this.move = function(){
-    if(keyIsDown(RIGHT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 0);
-      this.sprite.changeAnimation('moving-right');
-      this.sendPlayerData(3, false);
-      this.direction = 0;
-    }
+    this.playerName = game.add.text(0, 0, this.nick, this.nickStyle);
 
-    if(keyIsDown(DOWN_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 90);
-      this.sprite.changeAnimation('moving-down');
-      this.sendPlayerData(5, false);
-      this.direction = 90;
-    }
-
-    if(keyIsDown(LEFT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 180);
-      this.sprite.changeAnimation('moving-left');
-      this.sendPlayerData(7, false);
-      this.direction = 180;
-    }
-
-    if(keyIsDown(UP_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 270);
-      this.sprite.changeAnimation('moving-up');
-      this.sendPlayerData(1, false);
-      this.direction = 270;
-    }
-
-    if(keyIsDown(DOWN_ARROW) && keyIsDown(RIGHT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 45);
-      this.sprite.changeAnimation('moving-down-right');
-      this.sendPlayerData(4, false);
-      this.direction = 45;
-    }
-
-    if(keyIsDown(UP_ARROW) && keyIsDown(RIGHT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 315);
-      this.sprite.changeAnimation('moving-up-right');
-      this.sendPlayerData(2, false);
-      this.direction = 315;
-    }
-
-    if(keyIsDown(DOWN_ARROW) && keyIsDown(LEFT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 135);
-      this.sprite.changeAnimation('moving-down-left');
-      this.sendPlayerData(6, false);
-      this.direction = 135;
-    }
-
-    if(keyIsDown(UP_ARROW) && keyIsDown(LEFT_ARROW)){
-      this.sprite.setSpeed(playerSpeed, 225);
-      this.sprite.changeAnimation('moving-up-left');
-      this.sendPlayerData(8, false);
-      this.direction = 225;
-    }
-
-    //set the camera position to the players position
-    if (this.sprite.position.x > 0 && this.sprite.position.x < SCENE_W ){
-      camera.position.x = this.sprite.position.x;
-    }
-
-    if (this.sprite.position.y > 0 && this.sprite.position.y < SCENE_H){
-      camera.position.y = this.sprite.position.y;
-    }
-
-    // Set boundaries of the game
-    if(this.sprite.position.y > SCENE_H + windowHeight / 2 - 25){
-      this.sprite.position.y = SCENE_H + windowHeight / 2 - 25;
-    }
-
-    if(this.sprite.position.y < windowHeight / 2 * -1 + 25){
-      this.sprite.position.y = windowHeight / 2 * -1 + 25;
-    }
-
-    if(this.sprite.position.x > SCENE_W + windowWidth / 2 - 25){
-      this.sprite.position.x = SCENE_W + windowWidth / 2 - 25;
-    }
-
-    if(this.sprite.position.x < windowWidth / 2 * -1 + 25){
-      this.sprite.position.x = windowWidth / 2 * -1 + 25;
-    }
-
-    if (currentUser.nickname) {
-      fill(255, 255, 255);
-      textSize(10);
-      textAlign(CENTER);
-      text(currentUser.nickname, this.sprite.position.x, this.sprite.position.y - 20);
-    }
+    this.weapon = game.add.weapon(8, 'bullet');
+    this.weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
+    this.weapon.bulletSpeed = 600;
+    this.weapon.fireRate = 300;
+    this.weapon.trackSprite(this.sprite, 5, 5, false);
 
   }
 
-  this.changePosition = function(x, y, direction){
-    this.sprite.position.x = x;
-    this.sprite.position.y = y;
+  this.listenMovement = function(){
 
-    switch (direction) {
-      case 1:
-          this.sprite.changeAnimation('moving-up');
-        break;
-      case 2:
-          this.sprite.changeAnimation('moving-up-right');
-        break;
-      case 3:
-          this.sprite.changeAnimation('moving-right');
-        break;
-      case 4:
-          this.sprite.changeAnimation('moving-down-right');
-        break;
-      case 5:
-          this.sprite.changeAnimation('moving-down');
-        break;
-      case 6:
-          this.sprite.changeAnimation('moving-down-left');
-        break;
-      case 7:
-          this.sprite.changeAnimation('moving-left');
-        break;
-      case 8:
-          this.sprite.changeAnimation('moving-up-left');
-        break;
-      case 9:
-          this.sprite.changeAnimation('standing');
-        break;
-      default:
+    this.sprite.body.fixedRotation = true;
+    this.sprite.body.setZeroVelocity();
 
+    if (cursors.down.isDown) {
+        this.sprite.body.moveDown(300);
+    } else if (cursors.up.isDown) {
+        this.sprite.body.moveUp(300);
     }
 
-  }
-
-  // Stops all animations
-  this.keyReleased = function(){
-    this.sprite.setSpeed(0, 0);
-    this.sprite.changeAnimation('standing');
-    this.sendPlayerData(9, false);
-    this.direction = 0;
-    return false;
-  }
-
-  this.shoot = function(remote, direction){
-
-    var bullet = createSprite(this.sprite.position.x, this.sprite.position.y);
-    bullet.addImage(this.bulletImage);
-    bullet.setSpeed(9, this.direction);
-    bullet.life = 300;
-    if (remote) {
-      // nothing
-    }else{
-      this.sendPlayerData(this.direction, true);
+    if (cursors.right.isDown) {
+        this.sprite.body.moveRight(300);
+    } else if (cursors.left.isDown) {
+        this.sprite.body.velocity.x = -300;
     }
 
-  }
-
-  this.dash = function(){
-
-  }
-
-  this.sendPlayerData = function(direction, shooting){
-    var bullet = false;
-    if(shooting){
-      bullet = true;
+    if (cursors.left.isDown && !cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown) {
+        this.sprite.animations.play('walk-left', 11, false);
+        this.weapon.fireAngle = 180;
+    } else if (cursors.right.isDown && !cursors.up.isDown && !cursors.down.isDown && !cursors.left.isDown) {
+        this.sprite.animations.play('walk-right', 11, false);
+        this.weapon.fireAngle = 0;
     }
-    socket.emit('playerMovement', { x: this.sprite.position.x, y: this.sprite.position.y, dir: direction, nick: this.nickname, shooting: bullet });
+
+    if (cursors.down.isDown && cursors.right.isDown) {
+        this.sprite.animations.play('walk-down-right', 11, false);
+        this.weapon.fireAngle = 45;
+    } else if (cursors.down.isDown && cursors.left.isDown) {
+        this.sprite.animations.play('walk-down-left', 11, false);
+        this.weapon.fireAngle = 135;
+    } else if (cursors.down.isDown) {
+        this.sprite.animations.play('walk-down', 11, false);
+        this.weapon.fireAngle = 90;
+    }
+
+    if (cursors.up.isDown && cursors.right.isDown) {
+        this.sprite.animations.play('walk-up-right', 11, false);
+        this.weapon.fireAngle = 315;
+    } else if (cursors.up.isDown && cursors.left.isDown) {
+        this.sprite.animations.play('walk-up-left', 11, false);
+        this.weapon.fireAngle = 225;
+    } else if (cursors.up.isDown) {
+        this.sprite.animations.play('walk-up', 11, false);
+        this.weapon.fireAngle = 270;
+    }
+
+    if (fireButton.isDown) {
+        this.weapon.fire();
+    }
+
+    this.playerName.alignTo(this.sprite, Phaser.BOTTOM_CENTER, 0);
+
+  }
+
+  this.sendPlayerData = function(direction, shooting, nick, death, id){
+    socket.emit('playerMovement', {
+      direction: direction,
+      shooting: shooting,
+      nick: nick,
+      death: death,
+      id: id
+    });
   }
 
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,132 +1,59 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'war-of-the-old-omes',
-{ preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'war-of-the-old-omes', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
 
 function preload() {
-    game.load.image('background','assets/floor.jpg');
+    game.load.image('background', 'assets/floor.jpg');
     game.load.image('bullet', 'assets/bullet.png');
     game.load.spritesheet('ms', 'assets/player/sheet.png', 32, 32);
 }
 
 var player;
 var cursors;
-var playerName;
-var text1;
-var style = { font: "10px", fill: "#00ff44" };
-var weapon;
 var fireButton;
-var isAuth = false;
-
-function initGame(){
-
-}
 
 function create() {
-  game.add.tileSprite(0, 0, 1920, 1920, 'background');
-  game.world.setBounds(0, 0, 1920, 1920);
-  game.physics.startSystem(Phaser.Physics.P2JS);
-  player = game.add.sprite(game.world.centerX, game.world.centerY, 'ms');
-  text1 = game.add.text(0, 0, playerName, style);
-  player.animations.add('standing', [0,1], false);
-  player.animations.add('walk-down', [0,1,2,3,4,5], false);
-  player.animations.add('walk-left', [6,7,8,9,10,11], false);
-  player.animations.add('walk-right', [12,13,14,15,16,17], false);
-  player.animations.add('walk-up', [18,19,20,21,22,23], false);
-  player.animations.add('walk-down-left', [24,25,26,27,28,29], false);
-  player.animations.add('walk-down-right', [30,31,32,33,34,35], false);
-  player.animations.add('walk-up-left', [36,37,38,39,40,41], false);
-  player.animations.add('walk-up-right', [42,43,44,45,46,47], false);
-  game.physics.p2.enable(player);
-  //game.physics.arcade.enable(player);
-  player.body.fixedRotation = true;
-  weapon = game.add.weapon(8, 'bullet');
-  weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
-  weapon.bulletSpeed = 600;
-  weapon.fireRate = 300;
-  weapon.trackSprite(player, 5, 5, false);
 
-  cursors = game.input.keyboard.createCursorKeys();
-  game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    player = new Player(666, 'Player');
+    player.create();
+
+    game.physics.p2.enable(player.sprite);
+
     fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.camera.follow(player.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
     firebase.auth().onAuthStateChanged(function(user) {
 
-      if (user) {
-        playerName = user.displayName
+        if (user) {
+            
+            player.nick = user.displayName;
+            player.playerName.text = user.displayName;
+            $('#modalRegister').modal('hide');
+            $('#modalLogin').modal('hide');
 
-        console.log(playerName);
-        text1.text = playerName;
-        $('#modalRegister').modal('hide');
-        $('#modalLogin').modal('hide');
-      } else {
-        $('#modalRegister').modal('toggle');
-      }
+        } else {
+
+            $('#modalRegister').modal('toggle');
+
+        }
 
     });
-
-
 
 }
 
 function update() {
-
-    if (!isAuth) {
-        //return;
-    }
-
-    player.body.setZeroVelocity();
-
-    if (cursors.down.isDown) {
-      player.body.moveDown(300);
-    }else if(cursors.up.isDown){
-      player.body.moveUp(300);
-    }
-
-    if (cursors.right.isDown){
-      player.body.moveRight(300);
-    }else if(cursors.left.isDown){
-      player.body.velocity.x = -300;
-    }
-
-    if (cursors.left.isDown && !cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown) {
-      player.animations.play('walk-left', 11, false);
-      weapon.fireAngle = 180;
-    }else if(cursors.right.isDown && !cursors.up.isDown && !cursors.down.isDown && !cursors.left.isDown){
-      player.animations.play('walk-right', 11, false);
-      weapon.fireAngle = 0;
-    }
-
-    if (cursors.down.isDown && cursors.right.isDown){
-      player.animations.play('walk-down-right', 11, false);
-      weapon.fireAngle = 45;
-    }else if(cursors.down.isDown && cursors.left.isDown){
-      player.animations.play('walk-down-left', 11, false);
-      weapon.fireAngle = 135;
-    } else if(cursors.down.isDown){
-      player.animations.play('walk-down', 11, false);
-      weapon.fireAngle = 90;
-    }
-
-    if (cursors.up.isDown && cursors.right.isDown){
-      player.animations.play('walk-up-right', 11, false);
-      weapon.fireAngle = 315;
-    }else if(cursors.up.isDown && cursors.left.isDown){
-      player.animations.play('walk-up-left', 11, false);
-      weapon.fireAngle = 225;
-    } else if(cursors.up.isDown){
-      player.animations.play('walk-up', 11, false);
-      weapon.fireAngle = 270;
-    }
-
-    if (fireButton.isDown)
-    {
-        weapon.fire();
-    }
-
-    text1.alignTo(player, Phaser.BOTTOM_CENTER, 0);
-
+  player.listenMovement();
 }
 
 function render() {
-
     game.debug.text("Arrows to move.", 20, 20);
-
 }
